e8d37e2e88b3bb17db72890da2b230b902f81c9b
Jose Luis Campaña <jose-luis.campana.altran@axa-groupsolutions.com>
2017-01-03 11:16:51 +0100

    :lipstick: ES6 version

--- src/bot.js
+++ src/bot.js
@@ -1,24 +1,17 @@
 console.log('the bot is starting');
 
-var Twit = require('twit');
-var config = require('./config.json'); // to import the tw account info
-var t = new Twit(config); //better t because is an object not a class (Twit is a class t is an object of Twit class)
-
-
-// Tweet something cada rato
-//setInterval(tweetSomething, 1000*20); //20s
-//streamFollow();
-streamFavorite();
-
+const Twit = require('twit');
+const config = require('./config.json'); // to import the tw account info
+const t = new Twit(config); //better t because is an object not a class (Twit is a class t is an object of Twit class)
 
 function tweetSomething(tweetMsg){
 	//tweetMsg = 'hello world '+Math.floor(Math.random()*100);
 
-  var tweet = {
+  const tweet = {
     status: tweetMsg
   };
 
-  t.post('statuses/update', tweet, function(err, data, response){
+  t.post('statuses/update', tweet, (err, data, response) => {
     if (err){
 			console.log('Something went wrong');
 		} else{
@@ -30,39 +23,40 @@ function tweetSomething(tweetMsg){
 
 function getTweets(){
 	// parameters: dev.twitter.com/rest/reference/get/search/tweets
-	var params = {
+	const params = {
 		q: 'jovi', //query
 		count: 5 //num of tweets
   };
 
-  t.get('search/tweets', params, function (err, data, response){
-    var tweets = data.statuses;
-		//console.log(data); // all tweets and its info
-		for (var i=0; i<tweets.length; i++){
-			console.log(tweets[i].text); //only text of a tweet
-		}
+  t.get('search/tweets', params,  (err, data, response) => {
+    const tweets = data.statuses;
+
+    tweets.forEach((tweet) => {
+      console.log(tweet.text);
+    }, this);
+
   });
 }
 
 
 function streamFollow(){
-	var stream = t.stream('user');
+	const stream = t.stream('user');
 
 	//Anytime someone follows me
-  stream.on('follow', function(event) {
-    var name = event.source.name;
-		var screenName = event.source.screen_name;
-		tweetSomething('@'+screenName + " thanks for the follow");
+  stream.on('follow', (event) => {
+    const user = event.source;
+    tweetSomething(`@${user.screen_name} thanks for the follow`);
   });
 }
 
 function streamFavorite(){
-	var stream = t.stream('user');
+	const stream = t.stream('user');
 
 	//Anytime someone favs a tweet of mine
-  stream.on('favorite', function(event){
-    var name = event.source.name;
-		var screenName = event.source.screen_name;
-		tweetSomething('@'+screenName + " thanks for the fav");
+  stream.on('favorite', (event) => {
+    const user = event.source;
+    tweetSomething(`@${user.screen_name} thanks for the fav`);
   });
 }
+
+streamFavorite();
