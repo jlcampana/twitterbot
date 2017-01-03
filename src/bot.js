451679b9acd7c40f2df20f0ff7b4abe9edd60964
Jose Luis Campaña <jose-luis.campana.altran@axa-groupsolutions.com>
2017-01-03 11:36:01 +0100

    :wrench: some changes (endpoints and optional args)

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,11 @@ const Twit = require('twit');
 const config = require('./config.json'); // to import the tw account info
 const t = new Twit(config); //better t because is an object not a class (Twit is a class t is an object of Twit class)
 
+const PUBLISH = 'statuses/update';
+const SEARCH = 'search/tweets';
+const FAVORIZED = 'favorite';
+const FOLLOWED = 'follow';
+
 function tweetSomething(tweetMsg) {
   //tweetMsg = 'hello world '+Math.floor(Math.random()*100);
 
@@ -11,7 +16,7 @@ function tweetSomething(tweetMsg) {
     status: tweetMsg
   };
 
-  t.post('statuses/update', tweet, (err, data, response) => {
+  t.post(PUBLISH, tweet, (err, data, response) => {
     if (err) {
       console.log('Something went wrong');
     } else {
@@ -21,14 +26,15 @@ function tweetSomething(tweetMsg) {
   });
 }
 
-function getTweets() {
+//Default values for query and nTweets
+function getTweets(query = 'jovi', nTweets = 5) {
   // parameters: dev.twitter.com/rest/reference/get/search/tweets
   const params = {
-    q: 'jovi', //query
-    count: 5 //num of tweets
+    q: query,
+    count: nTweets
   };
 
-  t.get('search/tweets', params, (err, data, response) => {
+  t.get(SEARCH, params, (err, data, response) => {
     const tweets = data.statuses;
 
     tweets.forEach((tweet) => {
@@ -43,7 +49,7 @@ function streamFollow() {
   const stream = t.stream('user');
 
   //Anytime someone follows me
-  stream.on('follow', (event) => {
+  stream.on(FOLLOWED, (event) => {
     const user = event.source;
     tweetSomething(`@${user.screen_name} thanks for the follow`);
   });
@@ -53,7 +59,7 @@ function streamFavorite() {
   const stream = t.stream('user');
 
   //Anytime someone favs a tweet of mine
-  stream.on('favorite', (event) => {
+  stream.on(FAVORIZED, (event) => {
     const user = event.source;
     tweetSomething(`@${user.screen_name} thanks for the fav`);
   });
